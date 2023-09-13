0cc8a596a3453ef24483aec9b79e4e23fc6c8006
Riley <91571914+rilstrats@users.noreply.github.com>
2023-09-13 08:59:39 -0600

    added main.tf and proxmox.tf

---
+++ proxmox.tf
@@ -0,0 +1,19 @@
+variable "pm_api_url" {
+  type = string
+}
+variable "pm_api_token_id" {
+  type      = string
+  sensitive = true
+}
+variable "pm_api_token_secret" {
+  type      = string
+  sensitive = true
+}
+
+provider "proxmox" {
+  pm_api_url          = var.pm_api_url
+  pm_api_token_id     = var.pm_api_token_id
+  pm_api_token_secret = var.pm_api_token_secret
+  pm_tls_insecure     = true
+}
+
