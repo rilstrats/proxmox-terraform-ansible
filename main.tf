f7e818881caa64e54760ccf0e4282293e951d6f6
Riley <91571914+rilstrats@users.noreply.github.com>
2023-09-13 21:09:26 -0600

    added four teams with a db server and two workstations

--- main.tf
+++ main.tf
@@ -6,3 +6,27 @@ terraform {
     }
   }
 }
+
+resource "proxmox_vm_qemu" "db" {
+  count = 4
+  name = "team${count.index + 1}-db"
+  target_node = "pve-r430-1"
+
+  clone = "template-debian12"
+}
+
+resource "proxmox_vm_qemu" "workstation1" {
+  count = 4
+  name = "team${count.index + 1}-workstation1"
+  target_node = "pve-r430-1"
+
+  clone = "template-ubuntu2204"
+}
+
+resource "proxmox_vm_qemu" "workstation2" {
+  count = 4
+  name = "team${count.index + 1}-workstation2"
+  target_node = "pve-r430-1"
+
+  clone = "template-ubuntu2204"
+}
